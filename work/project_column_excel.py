90c6ff02a8dbab25317206bf06b9c0ab4ac58ec5
Fritte1982 <richard.bieligk@gmail.com>
2024-10-24 14:22:10 +0200

    closed projeck_column_excel.py

--- work/project_column_excel.py
+++ work/project_column_excel.py
@@ -7,3 +7,11 @@ in_xlsx_path:str = (
 output_xls = (
     r"C:\Users\richa\git-projekte\udemy-50-mini-projekte_python\output" +
     r"\output.xlsx")
+
+
+df = pd.read_excel(in_xlsx_path)
+
+df["Total"] = df["Price"] * df["Quantity"]
+print(df) 
+
+df.to_excel(output_xls, index=False)
