496b573ad53c534e39ed18419dcc893bdf9a8c88
Fritte1982 <richard.bieligk@gmail.com>
2024-10-24 13:49:42 +0200

    edit projeck_column_excel.py

---
+++ work/project_column_excel.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+in_xlsx_path:str = (
+    r"C:\Users\richa\git-projekte\udemy-50-mini-projekte_python\files_data\Input.xlsx"
+    )
+
+output_xls = (
+    r"C:\Users\richa\git-projekte\udemy-50-mini-projekte_python\output" +
+    r"\output.xlsx")
