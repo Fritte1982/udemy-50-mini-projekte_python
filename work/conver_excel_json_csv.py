6210a789338c8fe5a8c84e9b546301f7bb98b500
Fritte1982 <richard.bieligk@gmail.com>
2025-01-28 17:51:50 +0100

    ich kann den Editor nicht zum commiten übereden

---
+++ work/conver_excel_json_csv.py
@@ -0,0 +1,10 @@
+import  pandas as pd
+import json
+
+PATH_XLSX_IN:str = r"C:\Users\richa\git-projekte\udemy-50-mini-projekte_python\files_data\europe_1.xlsx"
+OUTPUT_PATH:str = r"C:\Users\richa\git-projekte\udemy-50-mini-projekte_python\output"
+json_out = OUTPUT_PATH + r"\europe.json"
+
+df = pd.read_excel(PATH_XLSX_IN)
+df.to_json(json_out,indent=4, orient="records")
+
