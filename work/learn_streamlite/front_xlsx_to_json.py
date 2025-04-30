719e05cb3c44aa405d60b18d7cfa65845703a52a
Fritte1982 <richard.bieligk@gmail.com>
2025-04-30 15:47:14 +0200

    frontend xlsx_to_json tutorial finshed

--- work/learn_streamlite/front_xlsx_to_json.py
+++ work/learn_streamlite/front_xlsx_to_json.py
@@ -1,11 +1,12 @@
-import sys
-import os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+"""Frontend das eine Excel-Datei in eine json wandelt und zum Download bereitstellt """
 import streamlit as st
-
-# Gehe zwei Ordner zurück vom aktuellen Dateipfad und füge den Pfad hinzu
-
-from xlsx_to_json_tutorial import xlx_to_json
+from xlsx_to_json_tutorial import xlsx_to_json
 
 st.title("Front XLSX to JSON")
 st.write("Upload a .xlsx file to convert it to JSON")
+uploaded_file = st.file_uploader("Upload a .xlsx file", type=["xlsx","xls"])
+if uploaded_file is not None:
+    generated_json = xlsx_to_json(uploaded_file)
+    st.json(generated_json)
+    st.download_button(label="Download JSON",file_name="converted.json",
+                       data=generated_json, mime="application/json")
--- work/learn_streamlite/xlsx_to_json_tutorial.py
+++ work/learn_streamlite/xlsx_to_json_tutorial.py
@@ -11,14 +11,14 @@ xlsx_dir = Path(pa.SOURCE_DIR) / "web_app_streamlit_sources"
 xlsx_in_path = xlsx_dir / "europe (2).xlsx"
 json_out_path = Path(pa.OUTPUT_PATH) / "europe_2.json"
 
-def xlx_to_json(xlsx_in):
+def xlsx_to_json(xlsx_in):
     df = pd.read_excel(xlsx_in)
     json_data = df.to_json(orient="records")
     return json_data
 
 
 def main():
-    pprint(xlx_to_json(xlsx_in_path))
+    pprint(xlsx_to_json(xlsx_in_path))
 
 if __name__ == '__main__':
     main()
