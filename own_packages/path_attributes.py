9093f26f8dd8b0a2213dc6e1a151115d0a49bfe9
Fritte1982 <richard.bieligk@gmail.com>
2025-08-08 16:09:26 +0200

    set a path to pathlib.Path-object in my own package

--- own_packages/path_attributes.py
+++ own_packages/path_attributes.py
@@ -7,7 +7,8 @@ PROJECT_PATH = path.dirname(WORK_PATH)
 SOURCE_DIR = path.join(PROJECT_PATH,"files_data")
 SOURCE_DIR = Path(SOURCE_DIR)
 SOURCE_XLSX= path.join(PROJECT_PATH, "files_data","employee_data (1).xlsx")
-OUTPUT_PATH: str = path.join(PROJECT_PATH,"output")
+OUTPUT_PATH = path.join(PROJECT_PATH,"output")
+OUTPUT_PATH = Path(OUTPUT_PATH)
 DOCX_SOURCE =  Path(SOURCE_DIR) /  "word_documents_sources"
 
 
